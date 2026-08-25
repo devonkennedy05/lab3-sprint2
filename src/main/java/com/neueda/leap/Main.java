de40e64fb6c2e44a2cf8581e6a3250781a5c1e6e
devonkennedy1005 <devonkennedy1005@gmail.com>
2026-08-25 09:39:28 -0400

    fix: edited the hello world for readability

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,6 +2,6 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! My name is Devon and I have conquered this repo!!!");
+        System.out.println("Hello world! My name is Devon and I have conquered this repo!!");
     }
 }
